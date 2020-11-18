21d1e5a8ec71b7f35791f7551e12df9449531b98
imaimai101 <imaikoo10150805@icloud.com>
2020-11-19 06:53:14 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+import 'mypage/mypage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyPage(),
+    );
+  }
+}
